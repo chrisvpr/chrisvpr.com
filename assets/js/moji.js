e009fc384887eb5def030b3d8c89133a1aaa128d
Chris Vargas <chzerox@gmail.com>
2020-05-28 14:51:15 -0400

    Canva

--- assets/js/moji.js
+++ assets/js/moji.js
@@ -2,4 +2,8 @@ if (window.location.href.indexOf("?ref=37") > -1) {
         document.querySelector('.cs--37').classList.add("--show");
         document.documentElement.style.setProperty('--red', '#40df6d');
 }
-    
+
+if (window.location.href.indexOf("?ref=12") > -1) {
+        document.querySelector('.cs--12').classList.add("--show");
+        document.documentElement.style.setProperty('--red', '#00c4cc');
+}
