9eae017f101ace115d76da42d34cda3f8e18508b
Chris Vargas <chzerox@gmail.com>
2020-05-28 12:42:28 -0400

    Customization

---
+++ assets/js/moji.js
@@ -0,0 +1,7 @@
+if (window.location.href.indexOf("?ref=73") > -1) {
+        document.querySelector('.cs--73').classList.add("--show");
+}
+if (window.location.href.indexOf("?ref=66") > -1) {
+    document.querySelector('.cs--66').classList.add("--show");
+}
+    
