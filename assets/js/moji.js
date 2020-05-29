0926f198eb9def781807c19bd04efa1fb51d8538
Chris Vargas <chzerox@gmail.com>
2020-05-29 11:55:35 -0400

    Smartly

--- assets/js/moji.js
+++ assets/js/moji.js
@@ -1,9 +1,17 @@
 if (window.location.href.indexOf("?ref=37") > -1) {
-        document.querySelector('.cs--37').classList.add("--show");
         document.documentElement.style.setProperty('--red', '#40df6d');
+        document.getElementById("moji-name").innerHTML +=  
+                "Let's design<span class='moji-comp'> the future <br>of transportation </span> 🚗.";         
 }
 
 if (window.location.href.indexOf("?ref=12") > -1) {
-        document.querySelector('.cs--12').classList.add("--show");
         document.documentElement.style.setProperty('--red', '#00c4cc');
+        document.getElementById("moji-name").innerHTML +=  
+                "Let's have <span class='moji-comp'>Crazy Big <br>Goals</span> together."; 
+}
+
+if (window.location.href.indexOf("?ref=114") > -1) {
+        document.documentElement.style.setProperty('--red', '#7d408c');
+        document.getElementById("moji-name").innerHTML +=  
+                "Hello <span class='moji-comp'> Humble Hungry Hunter</span>."; 
 }
